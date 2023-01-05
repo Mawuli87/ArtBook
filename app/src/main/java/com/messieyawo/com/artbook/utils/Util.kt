67e36fc8a74aa26ab69b321d83963c3dda684891
Koffi Nyonouglo <yawomessie@gmail.com>
2023-01-05 14:29:17 +0000

    Update Util.kt
    Add your own pixabay api key to run app

--- app/src/main/java/com/messieyawo/com/artbook/utils/Util.kt
+++ app/src/main/java/com/messieyawo/com/artbook/utils/Util.kt
@@ -2,7 +2,7 @@ package com.messieyawo.com.artbook.utils
 
 object Util {
 
-    const val API_KEY = "12718272-1b051c26becca1cb795e24cf0"
+    const val API_KEY = ""
     const val BASE_URL = "https://pixabay.com"
 
 }
